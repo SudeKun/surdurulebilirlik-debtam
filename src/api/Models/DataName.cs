78d39c6dc0c882ce32c9d0c94562815b257ac94a
Sude Kündüroğlu <sudekunduroglu@gmail.com>
2025-07-05 17:44:35 +0300

    the same

--- src/api/Models/DataName.cs
+++
@@ -1,9 +0,0 @@
-﻿amespace surdurulebilirlik_app.Models
-{
-    public class DataName
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}
